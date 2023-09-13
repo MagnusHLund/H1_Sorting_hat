94f823b29395d82f53f5439acc380ff03826e0bf
MagnusHLund <magnusherringelund@gmail.dk>
2023-09-13 09:26:02 +0200

    Add project files.

---
+++ H1_Sorting_hat/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace H1_Sorting_hat
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Creates an instance to the hat class
+            SortingHat hat = new SortingHat();
+
+            // Infinite loop, so all the students get sorted
+            // Because its Harry Potter, there magically appears infinite students
+            while (true)
+            {
+                // Writes what the hat.house returns and sleep for 0.1 second
+                Console.WriteLine(hat.House());
+                Thread.Sleep(100);
+            }
+        }
+
+    }
+}
---
+++ H1_Sorting_hat/SortingHat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Sorting_hat
+{
+    internal class SortingHat
+    {
+        // Enum that contains each house
+        public enum houses { Gryffindor, Hufflepuff, Ravenclaw, Slytherin }
+
+        /// <summary>
+        /// Method that gets called by the Main method, in class program.
+        /// This returns the enum values name, to he Main method
+        /// </summary>
+        /// <returns></returns>
+        public string House()
+        {
+            // Creates a random and gives a value between 0 and 3
+            Random rnd = new Random();
+            int value = rnd.Next(4);
+
+            // Gets the name of the enum, based on the random value.
+            // Returns to Main()
+            return Enum.GetName(typeof(houses), value);
+        }
+    }
+}
